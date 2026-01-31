57696f85e37f226674d32e724e3b391acbc073bc
Naif Alazmi <10932324@uvu.edu>
2026-01-30 22:32:14 -0700

    remove nitro from vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,15 @@
 import { defineConfig } from 'vite'
 import { devtools } from '@tanstack/devtools-vite'
-import viteReact from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-
 import { fileURLToPath, URL } from 'node:url'
-import { nitro } from 'nitro/vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [devtools(), nitro(), viteReact(), tailwindcss()],
+  plugins: [
+    devtools(),
+    react(),
+    tailwindcss(),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
